8d4b2c1ce6aa871b1475441854e5e8eee9382cfc
Nigarglm <tu7mk3dbf@code.edu.az>
2023-10-28 17:58:55 +0400

    added new task

---
+++ task_30oct.sql
@@ -0,0 +1,21 @@
+--CREATE DATABASE AB104
+
+--USE AB104
+
+--CREATE TABLE Students(
+--[Name] varchar NOT NULL,
+--Surname varchar DEFAULT 'XXX',
+--Age int CHECK(Age>16) , 
+--AvgPoint float CHECK(AvgPoint>0 and AvgPoint<=100),
+--)
+
+--INSERT INTO Students VALUES
+--('Nigar','Glm',19,95),
+--('Aysun','Ramazanova',18,86),
+--('Leyla','Rustemli',19,89),
+--('Ayshen','Jabbarli',20,93)
+
+--SELECT * FROM Students WHERE AvgPoint>51 
+--SELECT * FROM Students WHERE AvgPoint>51 and AvgPoint<90
+--SELECT * FROM Students WHERE [Name] like '%A' and [NAME] like 'n%'
+--SELECT * FROM Students WHERE AvgPoint<51 and Age>20
